c45b0407ecb7516c871ba396f5700f2fa7524adb
Maria Cowdrey <mariacowdrey@yahoo.com>
2022-03-16 17:25:02 -0700

    update routes and controllers

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,25 +1,8 @@
 const express = require('express')
 const router = express.Router()
+const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controllers/goalController')
 
-// Get goals
-router.get('/', (req, res) => {
-    res.status(200).json({ message: 'Get goals'})
-})
-
-// Create a goal
-router.post('/', (req, res) => {
-    res.status(200).json({ message: 'Set goal'})
-})
-
-// Update goal
-router.put('/:id', (req, res) => {
-    res.status(200).json({ message: `Update goal ${req.params.id}`})
-})
-
-// Delete goal
-router.delete('/:id', (req, res) => {
-    res.status(200).json({ message: `Delete goal ${req.params.id}`})
-})
-
+router.route('/').get(getGoals).post(setGoal)
+router.route('/:id').delete(deleteGoal).put(updateGoal)
 
 module.exports = router
