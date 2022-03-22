fc76981d5d92fa91e36e6865e6f5718d0faf9f98
Maria Cowdrey <mariacowdrey@yahoo.com>
2022-03-21 20:47:57 -0700

    jwt generate token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -37,7 +37,8 @@ const registerUser = asyncHandler(async (req, res) => {
         res.status(201).json({
             _id: user.id,
             name: user.name,
-            email: user.email
+            email: user.email,
+            token: generateToken(user._id)
         })
     } else {
         res.status(400)
@@ -59,6 +60,7 @@ const loginUser = asyncHandler(async (req, res) => {
             _id: user.id,
             name: user.name,
             email: user.email,
+            token: generateToken(user._id),
         })
     } else {
         res.status(400)
@@ -68,10 +70,17 @@ const loginUser = asyncHandler(async (req, res) => {
 
 // @desc Get user data
 // @route GET / api/users/me
-// @access Public
+// @access Private
 const getMe = asyncHandler(async (req, res) => {
     res.json({message: 'Display user data'})
 })
+
+// Generate JWT
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '30d',
+    })
+}
 
 
 module.exports = {
