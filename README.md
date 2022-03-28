0066b38afbd22e22bec849bb36700d585cabd5de
Maria Cowdrey <mariacowdrey@yahoo.com>
2022-03-28 16:52:15 -0700

    update readme and add goals

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # goal-setter
 
 ## MERN Stack:
-* Express & MongoDB Rest API
-* JWT Authentication
-* Frontend Authentication & Redux Toolkit
+
+- Express & MongoDB Rest API
+- JWT Authentication
+- Frontend Authentication & Redux Toolkit
+- Redux Goals
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,26 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
 function Dashboard() {
+  const navigate = useNavigate();
+
+  const { user } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (!user) {
+      navigate('/login');
+    }
+  }, [user, navigate]);
+
   return (
-    <div>Dashboard</div>
-  )
+    <>
+      <section className='heading'>
+        <h1>Welcome {user && user.name}</h1>
+        <p>Goals Dashboard</p>
+      </section>
+    </>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
